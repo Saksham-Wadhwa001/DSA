7deaa7184a692beac8a0c56c40432390a919c659
Saksham-Wadhwa001 <sakshamwadhwa1000@gmail.com>
2025-02-26 14:45:35 +0530

    Update 26.02.25.cpp

---
+++ 26.02.25.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+class Solution {
+    private:
+        void dfs(int n, vector<int>& v, vector<vector<int>>& isConnected) {
+            v[n] = 1;
+            for (int j = 0; j < isConnected.size(); j++) {
+                if (isConnected[n][j] == 1 && !v[j]) {
+                    dfs(j, v, isConnected);
+                }
+            }
+        }
+    
+    public:
+        int findCircleNum(vector<vector<int>>& isConnected) {
+            int n = isConnected.size();
+            vector<int> visit(n, 0);
+            int count = 0;
+    
+            for (int i = 0; i < n; i++) {
+                if (visit[i] == 0) {
+                    count++;
+                    dfs(i, visit, isConnected);
+                }
+            }
+            return count;
+        }
+    };
